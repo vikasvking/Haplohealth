da0ca1db49120c06b448ac843164d36f2a6e0325
vikasvking <vikas.vk711@gmail.com>
2017-05-04 12:22:25 +0530

    added sign in page xml

--- app/src/main/java/com/haplohealth/haplohealth/GettingStarted.java
+++ app/src/main/java/com/haplohealth/haplohealth/GettingStarted.java
@@ -1,7 +1,10 @@
 package com.haplohealth.haplohealth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GettingStarted extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class GettingStarted extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_getting_started);
+        final Button bGetStarted=(Button) findViewById(R.id.bGetStarted);
+        bGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent=new Intent(GettingStarted.this,SignInActivity.class);
+                startActivity(signInIntent);
+            }
+        });
     }
 }
