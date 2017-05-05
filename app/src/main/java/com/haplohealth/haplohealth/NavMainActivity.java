1dba329398950abc885d905a018aaa816b839f69
vikasvking <vikas.vk711@gmail.com>
2017-05-05 10:42:13 +0530

    starting navigation bar

--- app/src/main/java/com/haplohealth/haplohealth/NavMainActivity.java
+++ app/src/main/java/com/haplohealth/haplohealth/NavMainActivity.java
@@ -3,17 +3,11 @@ package com.haplohealth.haplohealth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class SignUpActivity extends AppCompatActivity {
-
-
+public class NavMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_sign_up);
-
+        setContentView(R.layout.activity_nav_main);
     }
 }
