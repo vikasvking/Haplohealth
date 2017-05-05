17f022f29867afca25f60cf22e5befb5aeea77aa
vikasvking <vikas.vk711@gmail.com>
2017-05-05 11:46:35 +0530

    added sign in

--- app/src/main/java/com/haplohealth/haplohealth/SignInActivity.java
+++ app/src/main/java/com/haplohealth/haplohealth/SignInActivity.java
@@ -1,18 +1,29 @@
 package com.haplohealth.haplohealth;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
     private EditText etEmail,etPassword;
     private TextView tvForgetPassword,tvSignUp,tvClinicLogIn;
     private Button bLogIn;
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "SignInActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +45,70 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
+
+        //authentication
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        bLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=etEmail.getText().toString();
+                String pass=etPassword.getText().toString();
+                if(!email.equals("")&&!pass.equals(""))
+                {
+                    signUp(email,pass);
+                }
+            }
+        });
+    }
+
+    private void signUp(String email,String pass) {
+        mAuth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            startActivity(new Intent(SignInActivity.this,NavMainActivity.class));
+                            finish();
+
+                        }
+                        else{
+                            toastMsg("invalid id or password");
+                        }
+                    }
+                });
+    }
+
+    private void toastMsg(String s) {
+        Toast toast=Toast.makeText(this,s,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
